9f315f6c5c561915ddb9eb5d112c24ec75a2e109
Mel/ <tchoikuemel06@gmail.com>
2024-04-29 12:08:31 +0100

    optimaze image folders in src/assets

--- src/books.js
+++ src/books.js
@@ -1,17 +1,28 @@
+import imag1 from '../src/assets/images/Mega-Menu-Vehicles-Model-S.png'
+import imag2 from '../src/assets/images/Mega-Menu-Vehicles-Model-X.png'
+import imag3 from '../src/assets/images/Mega-Menu-Vehicles-Model-Y.png'
+
 const books = [
 	{
 		id: 1,
 		author: 'Mel turham',
-		title: 'Tesla Model X',
-		img: '../src/assets/images/Mega-Menu-Vehicles-Model-X.png',
-		alt: 'tesla image model X',
+		title: 'Tesla Model S',
+		img:imag1,
+		alt: 'tesla image model S',
 	},
 	{
 		id: 2,
 		author: 'Houthou william',
-		title: 'Tesla Model S',
-		img: '../src/assets/images/Mega-Menu-Vehicles-Model-S.png',
-		alt: 'tesla image model S',
+		title: 'Tesla Model X',
+		img: imag2,
+		alt: 'tesla image model X',
+	},
+	{
+		id: 2,
+		author: 'Kouakam Olive',
+		title: 'Tesla Model Y',
+		img: imag3,
+		alt: 'tesla image model Y',
 	},
 ];
 
